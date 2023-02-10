a1deb0bced591de77def48617c3121479c584a57
David Ebbo <david.ebbo@gmail.com>
2023-02-09 19:54:39 -0800

    Change test tree

--- extract_minimal_tree_tests.py
+++ extract_minimal_tree_tests.py
@@ -3,12 +3,7 @@
 import unittest
 from extract_minimal_tree import extract
 
-Hominoidea = """(((Pongo_abelii_ott770295,Pongo_pygmaeus_ott770302:1.295194)Pongo:1.234,
-    (((Pan_troglodytes_ott417950,Pan_paniscus)Pan_ott417957,Homo_sapiens_ott770315)ChimpHumanClade,
-    (Gorilla_beringei_ott351685,Gorilla_gorilla_ott417965:2.345)Gorilla_ott417969)Homininae_ott312031)Hominidae_ott770311)Hominoidea_ott386191;"""
-
-# remove all spaces and newlines from the tree
-Hominoidea = Hominoidea.replace(" ", "").replace("\n", "")
+test_tree = "(A,(BA,((BBAA_ott123,BBAB,BBAC,BBAD)BAA,(BBBA)BBB,(BBCA:12.34,BBCB)BBC_ott456:78.9)BB)B,((CAA,CAB),CB)C,D)Root;"
 
 '''
 Unit test for extract_minimal_tree.extract
@@ -16,70 +11,82 @@ Unit test for extract_minimal_tree.extract
 class TestExtract(unittest.TestCase):
 
     def test_missing_taxa(self):
-        taxa = {"Canis", "Pan", "Tupaia"}
-        tree = extract(Hominoidea, taxa, expand_taxa=False)
+        taxa = {"X", "BBC", "Y"}
+        tree = extract(test_tree, taxa, expand_taxa=False)
 
         self.assertEqual(tree, None)
 
         self.assertEqual(
             taxa,
-            {"Canis", "Tupaia"}
+            {"X", "Y"}
         )
 
     def test_one_taxon_expanded(self):
-        taxa = {"Pan"}
-        tree = extract(Hominoidea, taxa, expand_taxa=True)
+        taxa = {"B"}
+        tree = extract(test_tree, taxa, expand_taxa=True)
 
         self.assertFalse(taxa)
-        self.assertEqual(tree, '(Pan_troglodytes_ott417950,Pan_paniscus)Pan_ott417957')
+        self.assertEqual(tree, '(BA,((BBAA_ott123,BBAB,BBAC,BBAD)BAA,(BBBA)BBB,(BBCA:12.34,BBCB)BBC_ott456:78.9)BB)B')
 
     def test_two_taxa(self):
-        taxa = {"Pan", "Pongo"}
-        tree = extract(Hominoidea, taxa, expand_taxa=False)
+        taxa = {"BA", "BBBA"}
+        tree = extract(test_tree, taxa, expand_taxa=False)
 
-        self.assertEqual(tree, '(Pongo:1.234,Pan_ott417957)Hominidae_ott770311')
+        self.assertEqual(tree, '(BA,BBBA)B')
+
+    def test_two_taxa_no_root_name(self):
+        taxa = {"CAA", "CAB"}
+        tree = extract(test_tree, taxa, expand_taxa=False)
+
+        self.assertEqual(tree, '(CAA,CAB)')
 
     def test_two_taxa_expanded(self):
-        taxa = {"Pan_troglodytes", "Pongo"}
-        tree = extract(Hominoidea, taxa, expand_taxa=True)
+        taxa = {"BBC", "C"}
+        tree = extract(test_tree, taxa, expand_taxa=True)
 
-        self.assertEqual(tree, '((Pongo_abelii_ott770295,Pongo_pygmaeus_ott770302:1.295194)Pongo:1.234,Pan_troglodytes_ott417950)Hominidae_ott770311')
+        self.assertEqual(tree, '((BBCA:12.34,BBCB)BBC_ott456:78.9,((CAA,CAB),CB)C)Root')
 
     def test_three_taxa(self):
-        taxa = {"Homo_sapiens", "Pan", "Pongo"}
-        tree = extract(Hominoidea, taxa)
+        taxa = {"BA", "C", "BBC"}
+        tree = extract(test_tree, taxa)
 
-        self.assertEqual(tree, '(Pongo:1.234,(Pan_ott417957,Homo_sapiens_ott770315)ChimpHumanClade)Hominidae_ott770311')
+        self.assertEqual(tree, '((BA,BBC_ott456:78.9)B,C)Root')
+
+    def test_three_taxa_polytomy(self):
+        taxa = {"BBAD", "BBAA", "BBAC"}
+        tree = extract(test_tree, taxa)
+
+        self.assertEqual(tree, '(BBAA_ott123,BBAC,BBAD)BAA')
 
     def test_two_nested_taxa(self):
-        taxa = {"Pan", "Hominidae"}
-        tree = extract(Hominoidea, taxa, expand_taxa=False)
+        taxa = {"B", "BBC"}
+        tree = extract(test_tree, taxa, expand_taxa=False)
 
-        self.assertEqual(tree, '(Pan_ott417957)Hominidae_ott770311')
+        self.assertEqual(tree, '(BBC_ott456:78.9)B')
 
     def test_two_nested_taxa_with_expansion(self):
-        taxa = {"Pan", "ChimpHumanClade"}
-        tree = extract(Hominoidea, taxa, expand_taxa=True)
+        taxa = {"BBC", "BB"}
+        tree = extract(test_tree, taxa, expand_taxa=True)
 
-        self.assertEqual(tree, '((Pan_troglodytes_ott417950,Pan_paniscus)Pan_ott417957,Homo_sapiens_ott770315)ChimpHumanClade')
+        self.assertEqual(tree, '((BBAA_ott123,BBAB,BBAC,BBAD)BAA,(BBBA)BBB,(BBCA:12.34,BBCB)BBC_ott456:78.9)BB')
 
     def test_three_nested_taxa(self):
-        taxa = {"Pan", "Hominidae", "Pan_paniscus"}
-        tree = extract(Hominoidea, taxa, expand_taxa=False)
+        taxa = {"BB", "BBC", "B"}
+        tree = extract(test_tree, taxa, expand_taxa=False)
 
-        self.assertEqual(tree, '((Pan_paniscus)Pan_ott417957)Hominidae_ott770311')
+        self.assertEqual(tree, '((BBC_ott456:78.9)BB)B')
 
     def test_nested_with_implied_taxon(self):
-        taxa = {"Hominidae", "Pan_paniscus", "Pan_troglodytes"}
-        tree = extract(Hominoidea, taxa, expand_taxa=False)
+        taxa = {"BBAB", "B", "BBAD"}
+        tree = extract(test_tree, taxa, expand_taxa=False)
 
-        self.assertEqual(tree, '((Pan_troglodytes_ott417950,Pan_paniscus)Pan_ott417957)Hominidae_ott770311')
+        self.assertEqual(tree, '((BBAB,BBAD)BAA)B')
 
     def test_mixed_scenarios(self):
-        taxa = {"Pongo", "Hominidae", "Pan_paniscus", "Gorilla_gorilla"}
-        tree = extract(Hominoidea, taxa, expand_taxa=False)
+        taxa = {"BBB", "B", "BBCA", "BBCB"}
+        tree = extract(test_tree, taxa, expand_taxa=False)
 
-        self.assertEqual(tree, '(Pongo:1.234,(Pan_paniscus,Gorilla_gorilla_ott417965:2.345)Homininae_ott312031)Hominidae_ott770311')
+        self.assertEqual(tree, '((BBB,(BBCA:12.34,BBCB)BBC_ott456:78.9)BB)B')
 
 
 unittest.main()
